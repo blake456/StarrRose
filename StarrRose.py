8d202c5c5e11646c330a6afea73d5eccc6d86a9f
blake456 <blake.alex456@gmail.com>
2023-07-10 14:28:02 +0100

    Adjusted comments on StarrRose.py

--- StarrRose.py
+++ StarrRose.py
@@ -4,7 +4,6 @@ import math
 import plotly.express as px
 from dash import Dash, dcc, html, Input, Output
 
-# now gonna turn this into a dash app
 app = Dash(__name__)
 
 app.layout = html.Div([
@@ -59,44 +58,35 @@ def StarrRose(a,b,c):
     petal_size = 0.3*width/2
     max = 1000
     
-    # the starr rose is a modification of the maurer rose:
-    # firstly, whereas the maurer rose has radial function r = sin(a*theta), the starr rose has r = 2 + 1/2sin(a*t) 
-    # where a is a fixed parameter and t is a variable angle parameter
-    # this means the radius oscillates between 3/2 and 5/2 instead of 0 and 1
-    # the angular function is stranger though as it is parameterised by t to generate "backward" movement
-    # we have theta(t) = t + sin(b*t)/c where b,c are fixed parameters
-    # the full parameterised function is (x(t),y(t)) = r(t) * (cos(theta(t), sin(theta(t))) 
-
     image = Image.new("RGB", (width, height), "white")
 
     draw = ImageDraw.Draw(image)
     points = []
 
     loops = 50
-
     for k in range(loops):
-
-        #scale = 1 - ((k+1)/loops)**2
         scale = (k+1)/loops
 
         for i in range(max):
             # find our angle parameter t in radians
             t = (2*math.pi * i) / max
 
+            # work out polar coordinate values
             r = 2 + 0.5 * math.sin(a*t)
-
             theta = t + math.sin(b*t)/c
 
+            # convert polar coordinate values to cartesian values
             xcoord = r * math.cos(theta)
             ycoord = r * math.sin(theta)
-            #now we need to translate these points so the rose is centered (and optionally scale them so we can see it)
+            
+            # translate and scale these points so the rose is centered and of a reasonable size
             points.append((width/2 + scale*petal_size*xcoord, height/2 + scale*petal_size*ycoord))
-        
+
+        # draw this loop, then clesr points for the next loop
         draw.line(points, width=1, fill="black", joint="curve")
         points = []
     
-
-    # convert image to a px figure for display purposes
+    # convert image to a px figure to display it
     fig = px.imshow(image)
     fig.update_layout(width=1200, height=1200)
     fig.update_xaxes(showticklabels=False).update_yaxes(showticklabels=False)
